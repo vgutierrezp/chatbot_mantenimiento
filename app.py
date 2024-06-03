e29f399032732e7c51b08d380918f4453b3f73c8
vgutierrezp <victor.gutierrez@ngr.com.pe>
2024-06-03 17:16:17 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,34 @@
 import streamlit as st
 from google.oauth2 import service_account
-from googleapiclient.discovery import build
+import gspread
 import pandas as pd
-import io
+from gspread_dataframe import get_as_dataframe
 
+# Autenticación de Google Sheets
 @st.experimental_singleton
 def authenticate():
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gdrive"],
-        scopes=["https://www.googleapis.com/auth/drive"]
-    )
-    return build('drive', 'v3', credentials=credentials)
+    credentials = service_account.Credentials.from_service_account_info(st.secrets["gdrive"])
+    client = gspread.authorize(credentials)
+    return client
 
 drive_service = authenticate()
 
-def load_data(folder_id):
-    results = drive_service.files().list(
-        q=f"'{folder_id}' in parents and mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'",
-        pageSize=10, fields="files(id, name)").execute()
-    items = results.get('files', [])
-
+# Cargar datos desde Google Sheets
+def load_data(sheet_id):
+    sheet = drive_service.open_by_key(sheet_id)
+    worksheet_list = sheet.worksheets()
     data = []
-    file_names = []
+    sheet_names = []
 
-    for item in items:
-        file_id = item['id']
-        file_name = item['name']
-        request = drive_service.files().get_media(fileId=file_id)
-        file = io.BytesIO(request.execute())
-        df = pd.read_excel(file)
+    for worksheet in worksheet_list:
+        df = get_as_dataframe(worksheet)
         data.append(df)
-        file_names.append(file_name)
+        sheet_names.append(worksheet.title)
 
-    return data, file_names
+    return data, sheet_names
 
-folder_id = '1uHfThsP-2xmFZzDBvQH_m1t6_QWfM6YT'
-data, file_names = load_data(folder_id)
+sheet_id = '1uHfThsP-2xmFZzDBvQH_m1t6_QWfM6YT'
+data, sheet_names = load_data(sheet_id)
 
 st.title("Chatbot de Mantenimiento")
 st.write("Este chatbot te ayudará a consultar información de mantenimiento preventivo.")
@@ -58,5 +51,5 @@ for i, df in enumerate(data):
         df = df[df['Familia'] == familia]
 
     if not df.empty:
-        st.write(f"Resultados de {file_names[i]}")
+        st.write(f"Resultados de {sheet_names[i]}")
         st.write(df)
